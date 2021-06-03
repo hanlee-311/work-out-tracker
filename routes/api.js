46dcb262d6b7f9c332f3d87ade951d2fb9343ff1
Hannah Lee <013hannah.a.lee@gmail.com>
2021-06-03 17:02:53 -0500

    added routes in api.js for getting workout data and workout range

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,48 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
 
+router.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {$addFields: {
+            totalDuration: {
+                $sum: '$exercises.duration'
+            }
+        }}
+    ]).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {$addFields: {
+            totalDuration: {
+                $sum: '$exercises.duration'
+            }
+        }}
+    ]).sort({_id:-1}).limit(7).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+});
+
+module.exports = router;
